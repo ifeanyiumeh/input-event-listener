f6ad593a24e60e23a1e356ff7af3d1b8fde1308d
Ifeanyi Umeh <somto_umeh@yahoo.com>
2022-03-02 00:28:54 -0500

    Completed task  to alert in broowser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [phrase, setPhrase] = useState("");
+
+  if (phrase === "open sesame") {
+    alert("YOU MAY ENTER BRO");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>What's the secret phrase?</h2>
+
+      <p>{phrase}</p>
+      <input
+        type="text"
+        value={phrase}
+        placeholder="Super duper secret"
+        onChange={(e) => setPhrase(e.target.value)}
+      />
+      <p>
+        Hint: It's <strong>open sesame</strong>
+      </p>
     </div>
   );
 }
